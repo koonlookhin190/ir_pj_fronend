8392a0cd2655f23148ca4848cc5c2f2034bac610
koonlookhin <krit_tipnuan@cmu.ac.th>
2023-01-06 04:26:08 +0700

    test login form

---
+++ src/views/user/LoginView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="login">
+    <h1>Login</h1>
+    <div class="form-inputs">
+      <label for="username">Username</label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        v-model="input.username"
+        placeholder="Username"
+      />
+    </div>
+    <div class="form-inputs">
+      <label for="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        v-model="input.password"
+        placeholder="Password"
+      />
+    </div>
+    <button type="button" v-on:click="login()">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+  data() {
+    return {
+      input: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#login .form-inputs {
+  padding-bottom: 10px;
+}
+
+#login .form-inputs label {
+  padding-right: 10px;
+}
+</style>
